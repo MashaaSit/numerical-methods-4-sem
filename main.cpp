4f7eefad0c3b8a41e4e156763ac38920ec5bcc06
MashaaSit <129902332+MashaaSit@users.noreply.github.com>
2025-02-13 12:07:08 +0300

    Add files via upload
    13 лаба

--- main.cpp
+++ main.cpp
@@ -1,142 +1,148 @@
 #include <iostream>
-#include <cmath>
 #include <fstream>
-#include <locale>
-#include <cstdlib>
-
+#include <cmath>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <iomanip>
 using namespace std;
 
-double f1(double x, double y, double z) {
-    return z;
+double f(double x, double y) {
+    return (3 * pow(x, 2) * exp(-x) - (x + 1) * y) / x;
 }
 
-double f2(double x, double y, double z) {
-    return (2 * log(x) - 2 * y - z) / x;
-}
+void adams_bashforth_fixed(double a, double b, double y0, int N, const string &filename) {
+    ofstream file(filename);
 
-double EulerMethod(double x0, double y0, double z0, double xn, double h, double eps, const string& filename) {
-    double y1, z1, y, z, x;
-    x = x0;
-    y = y0;
-    z = z0;
-    double error_y, error_z;
-    double y1_2h, z1_2h;
+    double y[7];
+    double x[7];
+    double f_vals[7];
+    f_vals[0] = f(x[0], y[0]);
 
-    ofstream final(filename);
+    double h = (b - a) / N;
+    y[0] = y0;
+    x[0] = a;
 
-    if (!final.is_open()) {
-        cerr << "Error opening file " << filename << endl;
-        return -1;
+    if (!file.is_open()) {
+        cerr << "Error opening file!" << endl;
+        return;
     }
 
-    if (eps == 0) {
-        final.precision(15);
-        final << x << "\t" << y << "\n";
+    file << x[0] << "\t" << y[0] << "\n";
 
-        while (x < xn) {
-            y1 = y + f1(x, y, z) * h;
-            z1 = z + f2(x, y, z) * h;
-            y = y1;
-            z = z1;
-            x += h;
-            final << x << "\t" << y << "\n";
+
+    for (int i = 1; i < 6; i++) {
+        x[i] = x[i - 1] + h;
+        double y_mid = y[i - 1] + h / 2 * f(x[i - 1], y[i - 1]);
+        y[i] = y[i - 1] + h * f(x[i - 1] + h / 2, y_mid);
+        file << x[i] << "\t" << y[i] << "\n";
+    }
+
+    while (true) {
+        if (x[5] > b) {
+            break;
         }
-    } else {
-        h = (xn - x0) * eps;
-        final << x << "\t" << y << "\t" << h << "\t" << eps << "\n";
 
-        do {
-            do {
-                y1 = y + f1(x, y, z) * h;
-                y1_2h = y + f1(x, y, z) * 2 * h;
-                z1 = z + f2(x, y, z) * h;
-                z1_2h = z + f2(x, y, z) * 2 * h;
-
-                error_y = fabs(y1_2h - y1);
-                error_z = fabs(z1_2h - z1);
-
-                if (error_y > eps || error_z > eps) {
-                    h *= 0.9;
-                } else if (error_z < eps * 0.95 || error_y < eps * 0.95) {
-                    h *= 1.1;
-                }
-                y = y1;
-                z = z1;
-            } while (error_z > eps && error_y > eps && (error_z < eps * 0.95 || error_y < eps * 0.95));
-
-            x += h;
-            final << x << "\t" << y << "\t" << h << "\t" << error_y << "\n";
-        } while (x < xn);
+        y[6] = y[5] + h / 24 * (9 * f_vals[5] + 19 * f_vals[4] - 5 * f_vals[3] + f_vals[2]);
+        x[6] = x[5] + h;
+        f_vals[6] = f(x[6], y[6]);
+        file << x[6] << "\t" << y[6] << "\n";
+        for (int i = 0; i < 6; i++) {
+            y[i] = y[i + 1];
+            x[i] = x[i + 1];
+            f_vals[i] = f_vals[i + 1];
+        }
     }
 
-    final.close();
-    return y;
+
+    file.close();
 }
 
-double shoot(double x0, double y0, double z0, double xn, double fv, double h, double eps, double a1, double b1, const string& filename) {
-    double x, y, z, y1, z1, theta, yl, zl;
-    x = x0;
-    y = y0;
-    z = z0;
-    theta = z0;
-    yl = z0;
-    int i = 0;
-    double c1 = (b1 + a1) * 0.5;
-    int steps = static_cast<int>((xn - x0) / h);
 
-    ofstream final(filename);
-
-    if (!final.is_open()) {
-        cerr << "Error opening file " << filename << endl;
-        return -1;
+void adams_bashforth(double a, double b, double y0, double eps, string &filename) {
+    ofstream file(filename);
+    if (!file.is_open()) {
+        cerr << "Error opening file!" << endl;
+        return;
     }
 
-    if (eps == 0) {
-        do {
-            zl = EulerMethod(x0, y0, c1, xn, h, eps, filename);
-            if (zl - fv > 0) {
-                b1 = c1;
-            } else if (zl - fv < 0) {
-                a1 = c1;
-            }
-            c1 = (b1 + a1) * 0.5;
-            i++;
-        } while (i < steps);
-    } else {
-        do {
-            zl = EulerMethod(x0, y0, c1, xn, h, eps, filename);
-            if (zl - fv > 0) {
-                b1 = c1;
-            } else if (zl - fv < 0) {
-                a1 = c1;
-            }
-            c1 = (b1 + a1) * 0.5;
-        } while (fabs(zl - fv) > eps);
+    const int max_steps = 7;
+    double y[max_steps];
+    double x[max_steps];
+    double f_vals[max_steps];
+    double h = (b - a) * eps;
+    y[0] = y0;
+    x[0] = a;
+    f_vals[0] = f(x[0], y[0]);
+
+    file << x[0] << "\t" << y[0] << "\t" << h << "\n";
+
+    for (int i = 1; i < 6; i++) {
+        x[i] = x[i - 1] + h;
+        double y_mid = y[i - 1] + h / 2 * f_vals[i - 1];
+        y[i] = y[i - 1] + h * f(x[i - 1] + h / 2, y_mid);
+        f_vals[i] = f(x[i], y[i]);
+        file << x[i] << "\t" << y[i] << "\t" << h << "\n";
     }
 
-    final.close();
-    return zl;
+    while (x[5] < b) {
+        double h_half = h / 2;
+
+        double y_mid_1 = y[5] + h_half / 24 * (9 * f_vals[5] + 19 * f_vals[4] - 5 * f_vals[3] + f_vals[2]);
+        double y_next_h_2 = y[5] + h_half / 24 * (
+                                9 * f(x[5] + h_half, y_mid_1) + 19 * f_vals[5] - 5 * f_vals[4]+ f_vals[3]);
+        double y_next_h = y[5] + h / 24 * (9 * f_vals[5] + 19 * f_vals[4] - 5 * f_vals[3] + f_vals[2]);
+
+        double runge_error = fabs(y_next_h - y_next_h_2) / 15.0;
+
+        if (runge_error < eps / 10) {
+            y[6] = y_next_h;
+            x[6] = x[5] + h;
+            f_vals[6] = f(x[6], y[6]);
+            file << x[6] << "\t" << y[6] << "\t" << h << "\n";
+            h = min(h * 1.5, 0.1);
+
+            for (int i = 0; i < 6; i++) {
+                y[i] = y[i + 1];
+                x[i] = x[i + 1];
+                f_vals[i] = f_vals[i + 1];
+            }
+        } else if (runge_error < eps) {
+            y[6] = y_next_h;
+            x[6] = x[5] + h;
+            f_vals[6] = f(x[6], y[6]);
+            file << x[6] << "\t" << y[6] << "\t" << h << "\n";
+
+            for (int i = 0; i < 6; i++) {
+                y[i] = y[i + 1];
+                x[i] = x[i + 1];
+                f_vals[i] = f_vals[i + 1];
+            }
+        } else {
+            h = max(h * 0.5, 1e-7);
+        }
+    }
+
+    file.close();
 }
+
 
 int main() {
+    double a = 1.0;
+    double y0 = 1 / exp(1);
+    double b = 5.0;
+    adams_bashforth_fixed(a, b, y0, 10, "adams_fixed_10.txt");
+    adams_bashforth_fixed(a, b, y0, 100, "adams_fixed_100.txt");
 
+    double eps = 0.1;
+    for (int i = 0; i < 7; i++) {
+        ostringstream stream;
+        stream << fixed << setprecision(8) << eps;
+        string filename = "adams_" + stream.str() + ".txt";
+        adams_bashforth(a, b, y0, eps, filename);
+        eps *= 0.1;
+    }
 
-    double x0 = 1;
-    double y0 = 0;
-    double z0 = 1; // Initial condition z0
-    double xn = 2;
-    double fv = log(2.0); // Target value at x = xn, which is log(2)
-    double h = 0.001;
-
-    shoot(x0, y0, z0, xn, fv, 0.1, 0, 0.83, 1.21, "fixed_10.txt");
-    shoot(x0, y0, z0, xn, fv, 0.01, 0, 0.83, 1.21, "fixed_100.txt");
-    shoot(x0, y0, z0, xn, fv, 0, 0.1, 0.83, 1.21, "eps_e-1.txt");
-    shoot(x0, y0, z0, xn, fv, 0, 0.01, 0.83, 1.21, "eps_e-2.txt");
-    shoot(x0, y0, z0, xn, fv, 0, 0.001, 0.83, 1.21, "eps_e-3.txt");
-    shoot(x0, y0, z0, xn, fv, 0, 0.0001, 0.83, 1.21, "eps_e-4.txt");
-    shoot(x0, y0, z0, xn, fv, 0, 0.00001, 0.83, 1.21, "eps_e-5.txt");
-    shoot(x0, y0, z0, xn, fv, 0, 0.000001, 0.83, 1.21, "eps_e-6.txt");
-    shoot(x0, y0, z0, xn, fv, 0, 0.0000001, 0.83, 1.21, "eps_e-7.txt");
 
     return 0;
 }
---
+++ y_exact.m
@@ -0,0 +1,3 @@
+function retval = y_exact(x)
+    retval = x.^2.* exp(-x);
+end
